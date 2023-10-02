a695b1cd9fd0a4ffcecfb1c0a65e46dd73e9aab4
RTerBorch <robin.ter.borch@gmail.com>
2023-10-02 16:00:47 +0200

    copyGroupProject

---
+++ src/main/java/com/example/DevOpsExaminationProject/DevOpsExaminationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.DevOpsExaminationProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsExaminationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsExaminationProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/DevOpsExaminationProject/controllers/CarController.java
@@ -0,0 +1,14 @@
+package com.example.DevOpsExaminationProject.controllers;
+
+import com.example.DevOpsExaminationProject.Car;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CarController {
+
+    @GetMapping("/car")
+    public Car getCar(){
+        return new Car();
+    }
+}
